c69725c67794bc97c797084ae94556c4a2e34582
Tobias Kuhn <kuhntobias@gmail.com>
2014-04-16 17:49:15 +0200

    Output different formats; refactoring

--- src/main/java/ch/tkuhn/nanopub/server/NanopubDb.java
+++ src/main/java/ch/tkuhn/nanopub/server/NanopubDb.java
@@ -2,11 +2,24 @@ package ch.tkuhn.nanopub.server;
 
 import java.net.UnknownHostException;
 
+import net.trustyuri.TrustyUriUtils;
+import net.trustyuri.rdf.CheckNanopub;
+
+import org.nanopub.Nanopub;
+import org.nanopub.NanopubImpl;
+import org.nanopub.NanopubUtils;
+import org.openrdf.rio.RDFFormat;
+
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 
 public class NanopubDb {
+
+	// Use trig internally to keep namespaces:
+	private static RDFFormat internalFormat = RDFFormat.TRIG;
 
 	private static NanopubDb obj;
 
@@ -42,6 +55,28 @@ public class NanopubDb {
 
 	public DBCollection getNanopubCollection() {
 		return db.getCollection("nanopubs");
+	}
+
+	public Nanopub getNanopub(String artifactCode) throws Exception {
+		BasicDBObject query = new BasicDBObject("id", artifactCode);
+		DBCursor cursor = NanopubDb.get().getNanopubCollection().find(query);
+		if (!cursor.hasNext()) {
+			return null;
+		}
+		String nanopubString = cursor.next().get("nanopub").toString();
+		return new NanopubImpl(nanopubString, internalFormat);
+	}
+
+	public void loadNanopub(Nanopub np) throws Exception {
+		if (!CheckNanopub.isValid(np)) {
+			throw new Exception("Nanopub doesn't have a valid trusty URI");
+		}
+		String artifactCode = TrustyUriUtils.getArtifactCode(np.getUri().toString());
+		String npString = NanopubUtils.writeToString(np, internalFormat);
+		BasicDBObject id = new BasicDBObject("id", artifactCode);
+		getNanopubCollection().remove(id);
+		BasicDBObject dbObj = id.append("nanopub", npString);
+		getNanopubCollection().insert(dbObj);
 	}
 
 }
--- src/main/java/ch/tkuhn/nanopub/server/NanopubServlet.java
+++ src/main/java/ch/tkuhn/nanopub/server/NanopubServlet.java
@@ -1,17 +1,15 @@
 package ch.tkuhn.nanopub.server;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.nanopub.Nanopub;
+import org.nanopub.NanopubUtils;
 import org.openrdf.rio.RDFFormat;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCursor;
 
 public class NanopubServlet extends HttpServlet {
 
@@ -19,28 +17,50 @@ public class NanopubServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String artifactCode = req.getServletPath().substring(1);
+		String r = req.getServletPath().substring(1);
+		boolean showInPlainText = false;
+		if (r.endsWith(".txt")) {
+			showInPlainText = true;
+			r = r.replaceFirst("\\.txt$", "");
+		}
+		String extension = "trig";
+		String artifactCode;
+		if (r.matches(".*\\.[a-z]{1,10}")) {
+			extension = r.replaceFirst("^.*\\.([a-z]{1,10})$", "$1");
+			artifactCode = r.replaceFirst("^(.*)\\.[a-z]{1,10}$", "$1");
+		} else {
+			artifactCode = r;
+		}
 		if (!artifactCode.matches("RA[A-Za-z0-9\\-_]{43}")) {
 			resp.sendError(400, "Invalid artifact code: " + artifactCode);
 			return;
 		}
-		String nanopubString;
+		Nanopub nanopub;
 		try {
-			BasicDBObject query = new BasicDBObject("id", artifactCode);
-			DBCursor cursor = NanopubDb.get().getNanopubCollection().find(query);
-			if (!cursor.hasNext()) {
-				resp.sendError(404, "Nanopub not found: " + artifactCode);
-				return;
-			}
-			nanopubString = cursor.next().get("nanopub").toString();
+			nanopub = NanopubDb.get().getNanopub(artifactCode);
 		} catch (Exception ex) {
 			resp.sendError(500, "Internal error: " + ex.getMessage());
 			return;
 		}
-		PrintWriter out = resp.getWriter();
-		out.println(nanopubString);
-		out.close();
-		resp.setContentType(RDFFormat.TRIG.getDefaultMIMEType());
+		RDFFormat format = RDFFormat.forFileName("np." + extension);
+		if (format == null) {
+			resp.sendError(400, "Unknown format: " + extension);
+			return;
+		} else if (NanopubUtils.isUnsuitableFormat(format)) {
+			resp.sendError(400, "Unsuitable RDF format: " + extension);
+			return;
+		}
+		if (showInPlainText) {
+			resp.setContentType("text/plain");
+		} else {
+			resp.setContentType(format.getDefaultMIMEType());
+		}
+		try {
+			NanopubUtils.writeToStream(nanopub, resp.getOutputStream(), format);
+		} catch (Exception ex) {
+			resp.sendError(500, "Internal error: " + ex.getMessage());
+		}
+		resp.getOutputStream().close();
 	}
 
 }
