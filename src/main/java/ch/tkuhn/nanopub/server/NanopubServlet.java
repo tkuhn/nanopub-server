ee3079cd400cd74afd1019d6d516fd51bcfd18a3
Tobias Kuhn <kuhntobias@gmail.com>
2014-05-05 18:01:02 +0200

    Support POST requests to add nanopubs

--- src/main/java/ch/tkuhn/nanopub/server/NanopubServlet.java
+++ src/main/java/ch/tkuhn/nanopub/server/NanopubServlet.java
@@ -7,6 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.trustyuri.TrustyUriUtils;
+
+import org.nanopub.Nanopub;
+import org.nanopub.NanopubImpl;
+import org.openrdf.rio.RDFFormat;
+
 public class NanopubServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -4542560440919522982L;
@@ -23,9 +29,33 @@ public class NanopubServlet extends HttpServlet {
 		} else if (r.getFullRequest().equals("/style/plain.css")) {
 			ResourcePage.show(r, resp, "style.css", "text/css");
 		} else {
-			resp.sendError(400, "Invalid request: " + r.getFullRequest());
+			resp.sendError(400, "Invalid GET request: " + r.getFullRequest());
 		}
 		resp.getOutputStream().close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ServerRequest r = new ServerRequest(req);
+		if (!r.isEmpty()) {
+			resp.sendError(400, "Invalid POST request: " + r.getFullRequest());
+		}
+		Nanopub np = null;
+		try {
+			np = new NanopubImpl(req.getInputStream(), RDFFormat.forMIMEType(req.getContentType(), RDFFormat.TRIG));
+		} catch (Exception ex) {
+			resp.sendError(400, "Error reading nanopub: " + ex.getMessage());
+		}
+		if (np != null) {
+			String code = TrustyUriUtils.getArtifactCode(np.getUri().toString());
+			try {
+				if (NanopubDb.get().getNanopub(code) == null) {
+					NanopubDb.get().loadNanopub(np);
+				}
+			} catch (Exception ex) {
+				resp.sendError(500, "Error storing nanopub: " + ex.getMessage());
+			}
+		}
 	}
 
 }
