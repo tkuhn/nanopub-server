530745bcba380d93cdb1dc5424ba41640273b68d
Tobias Kuhn <kuhntobias@gmail.com>
2014-05-16 11:21:17 +0200

    Load multi-nanopub files

--- src/main/java/ch/tkuhn/nanopub/server/LoadNanopubs.java
+++ src/main/java/ch/tkuhn/nanopub/server/LoadNanopubs.java
@@ -2,20 +2,28 @@ package ch.tkuhn.nanopub.server;
 
 import java.io.File;
 
-import org.nanopub.NanopubImpl;
+import org.nanopub.MultiNanopubRdfHandler;
+import org.nanopub.MultiNanopubRdfHandler.NanopubHandler;
+import org.nanopub.Nanopub;
+import org.openrdf.rio.RDFFormat;
 
 public class LoadNanopubs {
 
 	private LoadNanopubs() {}  // no instances allowed
 
-	public static void main(String[] args) {
-		try {
-			for (String s : args) {
-				NanopubDb.get().loadNanopub(new NanopubImpl(new File(s)));
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			System.exit(1);
+	public static void main(String[] args) throws Exception {
+		for (String filename : args) {
+			RDFFormat format = RDFFormat.forFileName(filename);
+			MultiNanopubRdfHandler.process(format, new File(filename), new NanopubHandler() {
+				@Override
+				public void handleNanopub(Nanopub np) {
+					try {
+						NanopubDb.get().loadNanopub(np);
+					} catch (Exception ex) {
+						throw new RuntimeException(ex);
+					}
+				}
+			});
 		}
 	}
 
