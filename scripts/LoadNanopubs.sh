62cb6f19adf323e11c54024bbb247b548351711b
Tobias Kuhn <kuhntobias@gmail.com>
2014-07-02 15:21:10 +0200

    Add auxiliary script 'exec-class.sh'

--- scripts/LoadNanopubs.sh
+++ scripts/LoadNanopubs.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-#
-# The following environment variable can be used:
-# - JAVA_OPTS: Can be used to set Java command line options
 
-CLASS=ch.tkuhn.nanopub.server.LoadNanopubs
-
-DIR=`pwd`
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-cd ..
-MEMETOOLSJAVADIR=`pwd`
-
-# for Cygwin:
-MEMETOOLSJAVADIR=${MEMETOOLSJAVADIR#/cygdrive/?}
-
-cd $DIR
-
-mvn -q -e -f $MEMETOOLSJAVADIR/pom.xml exec:java -Dexec.mainClass="$CLASS" -Dexec.args="$*"
+$( dirname "${BASH_SOURCE[0]}" )/exec-class.sh ch.tkuhn.nanopub.server.LoadNanopubs "$@"
