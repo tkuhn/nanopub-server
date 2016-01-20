20006608c40d2d9588c2a80dc205fd2ac28d5bdb
Tobias Kuhn <kuhntobias@gmail.com>
2016-01-20 14:22:07 +0100

    Increase protocol version number (enforcing UTF-8 now)

--- src/main/java/ch/tkuhn/nanopub/server/NanopubServerUtils.java
+++ src/main/java/ch/tkuhn/nanopub/server/NanopubServerUtils.java
@@ -4,7 +4,7 @@ public class NanopubServerUtils extends org.nanopub.extra.server.NanopubServerUt
 
 	// Version numbers have the form MAJOR.MINOR (for example, 0.12 is a newer version than 0.9!)
 
-	public static final String protocolVersion = "0.3";
+	public static final String protocolVersion = "0.4";
 	public static final int protocolVersionValue = getVersionValue(protocolVersion);
 
 	public static final String journalVersion = "0.3";
